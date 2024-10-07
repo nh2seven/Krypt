fef09c38ee2bb93fcc0bf0b35b123bd30271787f
nh2seven <nikhil.h@outlook.com>
2024-10-07 10:06:01 +0530

    Add groups module
    Module to handle CRUD operations for the group associated with credentials

---
+++ src/backend/groups.py
@@ -0,0 +1,58 @@
+import sqlite3 as sql
+
+
+class Group:
+    def __init__(self, db, group_id):
+        """
+        """
+        self.db = sql.connect(db, check_same_thread=True, cached_statements=32)
+        self.cur = self.db.cursor()
+        self.group_id = group_id
+
+    def create_group(self):
+        with self.db:
+            query = """
+                    CREATE TABLE grouped (
+                    group_id PRIMARY KEY UNIQUE
+                    , group_name TEXT NOT NULL
+                    );
+                    """
+            self.cur.execute(query)
+
+    def read_group(self):
+        with self.db:
+            query = f"""
+                    SELECT * FROM grouped
+                    WHERE group_id = ?;
+                    """
+            self.cur.execute(query, (self.group_id,))
+
+    def update_group(self, attr, value):
+        with self.db:
+            query = f"""
+                    UPDATE grouped
+                    SET {attr} = ?
+                    WHERE group_id = ?;
+                    """
+            self.cur.execute(query, (value, self.group_id,))
+
+    def delete_group(self): 
+        with self.db:
+            query = f"""
+                    DELETE FROM grouped
+                    WHERE group_id = ?;
+                    """
+            self.cur.execute(query, (self.group_id,))
+
+if __name__ == "__main__":
+    group_id = int(input("Group ID: "))
+    username = input("Username: ")
+    database = username.lower() + ".db"
+
+    group = Group(database, group_id)
+
+    # Test functions
+    group.create_group()
+    group.read_group()
+    group.update_group("group_name", "Test")
+    group.delete_group()
