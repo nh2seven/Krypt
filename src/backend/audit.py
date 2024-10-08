84031772d25abddae50ef8240e8a3c18bd5202a4
nh2seven <nikhil.h@outlook.com>
2024-10-08 11:25:25 +0530

    Add audit log module
    Added module to log events that happen within the app, to be modified later

---
+++ src/backend/audit.py
@@ -0,0 +1,62 @@
+import sqlite3 as sql
+
+
+class AuditLog:
+    def __init__(self, db, log_id):
+        """
+        """
+        self.db = sql.connect(db, check_same_thread=True, cached_statements=32)
+        self.cur = self.db.cursor()
+        self.log_id = log_id
+
+    def create_log(self):
+        with self.db:
+            query = """
+            CREATE TABLE auditlog (
+            log_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            action_type TEXT DEFAULT "Unknown",
+            details TEXT DEFAULT "N/A",
+            action_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+            """
+            self.cur.execute(query)
+
+    def read_log(self):
+        with self.db:
+            query = """
+            SELECT * FROM auditlog
+            WHERE log_id = ?;
+            """
+            self.cur.execute(query, (self.log_id,))
+
+    def update_log(self, action_type, details, action_time):
+        with self.db:
+            query = """
+            UPDATE auditlog
+            SET action_type = ?,
+                details = ?,
+                action_time = ?
+            WHERE log_id = ?;
+            """
+            self.cur.execute(query, (action_type, details, action_time, self.log_id))
+
+    def delete_log(self):
+        with self.db:
+            query = """
+            DELETE FROM auditlog
+            WHERE log_id = ?;
+            """
+            self.cur.execute(query, (self.log_id,))
+        pass
+
+
+if __name__ == "__main__":
+    database = "Audit.db"
+    db_path = "db/app/" + database
+    n = 1
+
+    audit = AuditLog(db_path, n)
+    audit.create_log()
+    audit.read_log()
+    audit.update_log("Action", "Details", "Time")
+    audit.delete_log()
