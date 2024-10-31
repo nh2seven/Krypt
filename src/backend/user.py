7d428068165833b046cded10a382823657d9a368
nh2seven <nikhil.h@outlook.com>
2024-10-31 20:27:21 +0530

    Update user database operations
    Rewrote user.py fully, added context manager for database connections

--- src/backend/user.py
+++ src/backend/user.py
@@ -1,191 +1,87 @@
+from init import InitUser as iu
 import sqlite3 as sql
+from contextlib import contextmanager
 
 
-# Handle a single user database
+@contextmanager
+def db_connect(database):
+    connection = sql.connect(database, check_same_thread=False, cached_statements=32)
+    cursor = connection.cursor()
+    try:
+        yield cursor
+    finally:
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+
 class Credentials:
-    def __init__(self, db, username, password):
+    def __init__(self, db):
         """
-        Initializes variables for a single user database
+        Handle user credentials.
 
         Args:
-            self.db: Implicitly creates a new database if it doesn't exist
-            self.cur: Cursor object to interact with the database
-            self.uname: Username for the user
-            self.pw: Password for the user
+        db (str): Database path.
         """
-        self.db = sql.connect(db, check_same_thread=True, cached_statements=32)
-        self.cur = self.db.cursor()
-        self.user = username
-        self.pw = password
+        self.db = db
 
-    def create_cred(self):
-        with self.db:
-            query = """
-                    CREATE TABLE IF NOT EXISTS credentials (
-                    cred_name TEXT PRIMARY KEY UNIQUE,
-                    username TEXT NOT NULL UNIQUE CHECK(length(username) >= 8),
-                    password TEXT NOT NULL CHECK(length(password) >= 8),
-                    website TEXT,
-                    meta_id INTEGER,
-                    group_id INTEGER,
-                    FOREIGN KEY (meta_id) REFERENCES metadata(meta_id),
-                    FOREIGN KEY (group_id) REFERENCES grouped(group_id)
-                    );
-                    """
-            self.cur.execute(query)
+    def create_user(self):  # Pass user database name/path as argument
+        user = iu(self.db)
+        user.init_cred()
 
-    def read_cred(self):
-        with self.db:
-            query = f"""
-                    SELECT * FROM credentials
-                    WHERE cred_name = ?;
-                    """
-            self.cur.execute(query, (self.user,)) # Prevent SQL injection
+    def add_cred(
+        self, title, username, password, url, notes, tags, expiration, group_id
+    ):
+        with db_connect(self.db) as cur:
+            query = """INSERT INTO credentials (title, username, password, url, notes, tags, expiration, group_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?);"""
+            params = (title, username, password, url, notes, tags, expiration, group_id)
+            cur.execute(query, params)
 
-    def update_cred(self, attr, value):
-        with self.db:
-            query = f"""
-                    UPDATE credentials
-                    SET {attr} = ?
-                    WHERE cred_name = ?;
-                    """
-            self.cur.execute(query, (value, self.user,))
+    def get_cred(self, title):
+        with db_connect(self.db) as cur:
+            query = """SELECT * FROM credentials WHERE title = ?;"""
+            cur.execute(query, (title,))
 
-    def delete_cred(self):
-        with self.db:
-            query = f"""
-                    DELETE FROM credentials
-                    WHERE cred_name = ?;
-                    """
-            self.cur.execute(query, (self.user,))
+    def modify_cred(
+        self, title, username, password, url, notes, tags, expiration, group_id
+    ):
+        with db_connect(self.db) as cur:
+            query = """UPDATE credentials SET username = ?, password = ?, url = ?, notes = ?, tags = ?, expiration = ?, group_id = ? WHERE title = ?;"""
+            params = (username, password, url, notes, tags, expiration, group_id, title)
+            cur.execute(query, params)
+
+    def remove_cred(self, title):
+        with db_connect(self.db) as cur:
+            query = """DELETE FROM credentials WHERE title = ?;"""
+            cur.execute(query, (title,))
+
+    def delete_user(self):
+        with db_connect(self.db) as cur:
+            query = """DROP DATABASE ?;"""
+            cur.execute(query, (self.db,))
 
 
-class Group:
-    def __init__(self, db, group_id):
+class Groups:
+    def __init__(self, db):
         """
-        Initializes the group for a single user database
+        Handle user groups.
 
         Args:
-            self.db: Implicitly creates a new database if it doesn't exist
-            self.cur: Cursor object to interact with the database
-            self.group_id: Group ID for the group associated with a user's credentials
+        db (str): Database path.
         """
-        self.db = sql.connect(db, check_same_thread=True, cached_statements=32)
-        self.cur = self.db.cursor()
-        self.group_id = group_id
+        self.db = db
 
     def create_group(self):
-        with self.db:
-            query = """
-                    CREATE TABLE grouped (
-                    group_id PRIMARY KEY UNIQUE
-                    , group_name TEXT NOT NULL
-                    );
-                    """
-            self.cur.execute(query)
+        group = iu(self.db)
+        group.init_cred()
 
-    def read_group(self):
-        with self.db:
-            query = f"""
-                    SELECT * FROM grouped
-                    WHERE group_id = ?;
-                    """
-            self.cur.execute(query, (self.group_id,))
-
-    def update_group(self, attr, value):
-        with self.db:
-            query = f"""
-                    UPDATE grouped
-                    SET {attr} = ?
-                    WHERE group_id = ?;
-                    """
-            self.cur.execute(query, (value, self.group_id,))
-
-    def delete_group(self): 
-        with self.db:
-            query = f"""
-                    DELETE FROM grouped
-                    WHERE group_id = ?;
-                    """
-            self.cur.execute(query, (self.group_id,))
+    def delete_group(self, group_id):
+        with db_connect(self.db) as cur:
+            query = """DELETE FROM groups WHERE group_id = ?;"""
+            cur.execute(query, (group_id,))
 
 
-class Metadata:
-    def __init__(self, db, meta_id):
-        """
-        Initializes the metadata associated with a single user database
-
-        Args:
-            self.db: Implicitly creates a new database if it doesn't exist
-            self.cur: Cursor object to interact with the database
-            self.meta_id: Metadata ID for the metadata associated with a user's credentials
-        """
-        self.db = sql.connect(db, check_same_thread=True, cached_statements=32)
-        self.cur = self.db.cursor()
-        self.meta_id = meta_id
-
-    def create_meta(self):
-        with self.db:
-            query = """
-                    CREATE TABLE metadata (
-                    meta_id INTEGER PRIMARY KEY UNIQUE,
-                    description TEXT DEFAULT "N/A",
-                    tags TEXT DEFAULT "N/A",
-                    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                    expire_in TIMESTAMP NOT NULL
-                    );
-                    """
-            self.cur.execute(query)
-        
-    def read_meta(self):
-        with self.db:
-            query = f"""
-                    SELECT * FROM metadata
-                    WHERE meta_id = ?;
-                    """
-            self.cur.execute(query, (self.meta_id,))
-
-    def update_meta(self, attr, value):
-        with self.db:
-            query = f"""
-                    UPDATE metadata
-                    SET {attr} = ?
-                    WHERE meta_id = ?;
-                    """
-            self.cur.execute(query, (value, self.meta_id,))
-
-    def delete_meta(self):
-        with self.db:
-            query = f"""
-                    DELETE FROM metadata
-                    WHERE meta_id = ?;
-                    """
-            self.cur.execute(query, (self.meta_id,))
-
-
-if __name__ == "__main__":
-    username = input("Username: ")
-    password = input("Password: ")
-    database = username.lower() + ".db"
-    meta_id = int(input("Metadata ID: "))
-    group_id = int(input("Group ID: "))
-
-    user = Credentials(database, username, password)
-    meta = Metadata(database, meta_id)
-    group = Group(database, group_id)
-
-    # Test functions
-    user.create_cred()
-    user.read_cred()
-    user.update_cred("cred_name", "xyz")
-    user.delete_cred()
-
-    meta.create_meta()
-    meta.read_meta()
-    meta.update_meta("description", "Test")
-    meta.delete_meta()
-
-    group.create_group()
-    group.read_group()
-    group.update_group("group_name", "Test")
-    group.delete_group()
+if __name__ == "__main__":  # Dummy code, to be replaced
+    database = "user.db"
+    cred = Credentials(database)
+    group = Groups(database)
