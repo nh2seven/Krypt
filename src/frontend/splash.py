c66bbba2e7ab19fd9d9e89bbb96c2e9cf577c377
nh2seven <nikhil.h@outlook.com>
2024-11-05 20:44:09 +0530

    Add splash screen widget
    Created widget for splash screen on initial startup

---
+++ src/frontend/splash.py
@@ -0,0 +1,45 @@
+import sys
+
+from qfluentwidgets import SplashScreen
+from qframelesswindow import FramelessWindow, StandardTitleBar
+from PyQt6.QtCore import QEventLoop, QTimer, QSize
+from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import QApplication
+
+
+class Splash(FramelessWindow):
+    def __init__(self):
+        super().__init__()
+        self.resize(1000, 600)
+        self.setWindowTitle("Krypt")
+        self.setWindowIcon(QIcon("assets/splash.png"))
+
+        # 1. Create a splash screen
+        self.splashScreen = SplashScreen(self.windowIcon(), self)
+        self.splashScreen.setIconSize(QSize(102, 102))
+
+        titleBar = StandardTitleBar(self.splashScreen)
+        titleBar.setIcon(self.windowIcon())
+        titleBar.setTitle(self.windowTitle())
+        self.splashScreen.setTitleBar(titleBar)
+
+        # 2. Show the main interface before creating other sub-interfaces
+        self.show()
+
+        # 3. Create sub-interfaces
+        self.createSubInterface()
+
+        # 4. Hide the splash screen
+        self.splashScreen.finish()
+
+    def createSubInterface(self):
+        loop = QEventLoop(self)
+        QTimer.singleShot(3000, loop.quit)  # 3 seconds to load other stuff
+        loop.exec()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    w = Splash()
+    w.show()
+    app.exec()
