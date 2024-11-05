4e5609aafcf18a1ed06011303be2fbf397703527
nh2seven <nikhil.h@outlook.com>
2024-11-05 21:12:23 +0530

    Clean out main.py
    main.py to be used later as entry point for the entire application, not only front-end

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+# Import necessary modules
+import sys
+
+# Unify all modules
 from src.backend import app, user
 from src.modules import encryption, login, pw_gen
+from src.frontend import base, cred, log_reg, sidebar, view
 
-def main():
-    pass
-
-if __name__ == "__main__":
-    pass
