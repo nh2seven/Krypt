e089839b82d66a16c6f5b87378cb903500e6ad68
nh2seven <nikhil.h@outlook.com>
2024-11-11 09:49:14 +0530

    Update main to run splash screen
    Splash screen tied in with main.py, transitions to main app interface

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 # Import necessary modules
 import sys
+from PyQt6.QtCore import QTimer
+from PyQt6.QtWidgets import QApplication
+from src.frontend.splash import Splash  # Import Splash class
+from src.frontend.base import Window  # Import the main application window
 
 # Unify all modules
 from src.backend import app, user
 from src.modules import encryption, login, pw_gen
-from src.frontend import base, cred, log_reg, sidebar, view
+from src.frontend import base, cred, log_reg, sidebar
 from PyQt6.QtWidgets import QApplication
 
-if __name__ == "__main__":
+def runApp():
     app = QApplication(sys.argv)
-    window = base.Window()
-    window.show()
-    app.exec()
+
+    splash = Splash()
+    splash.show()
+
+    QTimer.singleShot(400, lambda: showMainWindow(splash))
+    sys.exit(app.exec())
+
+def showMainWindow(splash):
+    splash.close()  # Close the splash screen
+    mainWindow = Window()  # Initialize the main window
+    mainWindow.show()  # Show the main application window
+
+if __name__ == "__main__":
+    runApp()
