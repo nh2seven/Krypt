7c23d6215b53773fe87b0a67e259cda9935a34ed
nh2seven <nikhil.h@outlook.com>
2024-11-11 09:36:14 +0530

    Update main.py
    Updated to call front-end from application entry point

--- main.py
+++ main.py
@@ -5,4 +5,10 @@ import sys
 from src.backend import app, user
 from src.modules import encryption, login, pw_gen
 from src.frontend import base, cred, log_reg, sidebar, view
+from PyQt6.QtWidgets import QApplication
 
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = base.Window()
+    window.show()
+    app.exec()
