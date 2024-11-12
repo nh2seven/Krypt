bccdfd9982c8527dd78d4a6c221311ce228b8623
nh2seven <nikhil.h@outlook.com>
2024-11-12 11:17:50 +0530

    Update main.py
    Removed redundant few comments and 1 import

--- main.py
+++ main.py
@@ -2,14 +2,13 @@
 import sys
 from PyQt6.QtCore import QTimer
 from PyQt6.QtWidgets import QApplication
-from src.frontend.splash import Splash  # Import Splash class
-from src.frontend.base import Window  # Import the main application window
+from src.frontend.splash import Splash
+from src.frontend.base import Window
 
 # Unify all modules
 from src.backend import app, user
 from src.modules import encryption, login, pw_gen
 from src.frontend import base, cred, log_reg, sidebar
-from PyQt6.QtWidgets import QApplication
 
 def runApp():
     app = QApplication(sys.argv)
@@ -21,9 +20,9 @@ def runApp():
     sys.exit(app.exec())
 
 def showMainWindow(splash):
-    splash.close()  # Close the splash screen
-    mainWindow = Window()  # Initialize the main window
-    mainWindow.show()  # Show the main application window
+    splash.close()
+    mainWindow = Window()
+    mainWindow.show()
 
 if __name__ == "__main__":
     runApp()
