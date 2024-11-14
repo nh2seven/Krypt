37b72c215b736ffa76082b783e564cacb310096e
nh2seven <nikhil.h@outlook.com>
2024-11-14 19:24:48 +0530

    Add setup function

--- main.py
+++ main.py
@@ -1,42 +1,49 @@
-# main.py
 import sys
 import os
 from PyQt6.QtWidgets import QApplication
-from PyQt6.QtGui import QIcon
 
 # Unify all modules
 from src.backend import app, user
 from src.modules import auth, encryption, pw_gen
 from src.frontend import base, login
+import setup
+
 
 # main.py
 class Application:
     def __init__(self):
         self.app = QApplication(sys.argv)
         self.app.setDesktopFileName("krypt")
-        
+
         # Create single instances
         self.login = login.LoginScreen()
         self.main_window = base.MainWindow()
-        
+
         # Connect signals
         self.login.login_success.connect(self.show_main_window)
         self.main_window.logout.connect(self.show_login)
-        
+
     def show_login(self):
         self.main_window.hide()
-        self.login.clear_fields()  # Add this method to LoginScreen
+        self.login.clear_fields()
         self.login.show()
-        
+
     def show_main_window(self):
         self.login.hide()
         self.main_window.refresh_credentials()
         self.main_window.show()
-        
+
     def run(self):
         self.show_login()
         return self.app.exec()
 
+
+def Setup():
+    if not os.path.exists("db"):
+        setup.dirs()
+
+
 if __name__ == "__main__":
+    Setup()
     app = Application()
     sys.exit(app.run())
