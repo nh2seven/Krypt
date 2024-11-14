5bd4a0830eb4c721c23b30df0f5146f99bcb8193
nh2seven <nikhil.h@outlook.com>
2024-11-14 21:35:22 +0530

    Update login authentication
    Add modular login mechanism for regular users

--- src/backend/auth.py
+++ src/backend/auth.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3 as sql
 from contextlib import contextmanager
+from .init import InitUser
 
 
 @contextmanager
@@ -25,7 +26,7 @@ class Admin:
             password (str): Password for the admin.
         """
         self.db = db
-        self.uname = username # 1
+        self.uname = username  # 1
         self.pw = password  # Encryption to be applied in an external module
 
     def login(self):
@@ -41,6 +42,48 @@ class Admin:
         """Handles admin logout. Encrypt data before logging out."""
         print(f"{self.uname} logged out")
         # Redirect to login screen
+
+
+class User:
+    def __init__(self, db, username, password):
+        """
+        Initializes the variables for a user.
+
+        Args:
+            username (str): Username for the user.
+            password (str): Password for the user.
+        """
+        self.db = db
+        self.uname = username
+        self.pw = password  # Encryption to be applied in an external module
+
+    def create(self, pw):
+        """Creates a new user database."""
+        user = InitUser(self.db)  # Pass the user database name from frontend
+        user.init_user(pw)
+        user.init_cred()
+        user.init_group()
+
+    def login(self):
+        """Verifies user login details."""
+        with db_connect(self.db) as cur:
+            query = "SELECT pwd FROM user"
+            cur.execute(query)
+            result = cur.fetchone()
+            return result and result[0] == self.pw
+            # Add redirect to user panel
+
+    def logout(self):
+        """Handles user logout"""
+        print(f"{self.uname} logged out")
+        # Redirect to login screen; encrypt data before logging out
+
+    def delete(self):
+        """Deletes the user database"""
+        db_path = "db/users/" + self.db
+        if os.path.exists(db_path):
+            os.remove(self.db)
+            # Redirect to login screen, print message
 
 
 if __name__ == "__main__":
