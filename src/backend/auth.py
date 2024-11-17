1223d9836cd87d2c596f78f9a7fc8c1f47aa163b
nh2seven <nikhil.h@outlook.com>
2024-11-17 20:55:13 +0530

    Remove admin authentication

--- src/backend/auth.py
+++ src/backend/auth.py
@@ -3,34 +3,6 @@ from .init import InitUser
 from src.modules.contextmanager import db_connect
 
 
-class Admin:
-    def __init__(self, db, username, password):
-        """
-        Initializes the variables for an admin.
-
-        Args:
-            username (int): Username for the admin.
-            password (str): Password for the admin.
-        """
-        self.db = db
-        self.uname = username  # 1
-        self.pw = password  # Encryption to be applied in an external module
-
-    def login(self):
-        """Verify login details for admin."""
-        with db_connect(self.db) as cur:
-            query = "SELECT admin_pw FROM admin WHERE admin_id = ?"
-            cur.execute(query, (self.uname,))
-            result = cur.fetchone()
-            if result and result[0] == self.pw:
-                pass  # Redirect to admin panel
-
-    def logout(self):
-        """Handles admin logout. Encrypt data before logging out."""
-        print(f"{self.uname} logged out")
-        # Redirect to login screen
-
-
 class User:
     def __init__(self, db, username, password):
         """
