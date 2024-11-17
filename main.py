ab7a54a550d112ce92090431cfa58225f3e85cdc
nh2seven <nikhil.h@outlook.com>
2024-11-17 18:40:14 +0530

    Remove admin setup

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
 import sys
-import os
 from PyQt6.QtWidgets import QApplication
-
-# Unify all modules
-from src.backend import app, auth, user, init
 from src.frontend import base, login
 import setup
 
 
-# main.py
 class Application:
     def __init__(self):
         self.app = QApplication(sys.argv)
         self.app.setDesktopFileName("krypt")
 
-        # Create single instances
+        setup.dirs()
+
         self.login = login.LoginScreen()
         self.main_window = base.MainWindow()
-
-        # Connect signals
         self.login.login_success.connect(self.show_main_window)
         self.main_window.logout.connect(self.show_login)
 
@@ -37,20 +31,6 @@ class Application:
         return self.app.exec()
 
 
-def Setup():
-    if os.path.exists("db"):
-        pass
-    else:
-        print("Setting up Krypt...")
-        
-        db_path = "db/App.db"
-        setup.dirs()
-        krypt = init.InitApp(db_path)   
-        krypt.init_tables()
-        setup.set_admin(db_path)
-
-
 if __name__ == "__main__":
-    Setup()
     app = Application()
     sys.exit(app.run())
